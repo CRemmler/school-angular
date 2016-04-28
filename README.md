e3f3f13417ce87877fb3072464e133d424839acb
CRemmler <carolyn@remmler.org>
2016-04-28 09:45:34 -0500

    [choose school]

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# school-angular
+Objective:
+Create a webpage that manages a school classroom.
+
+User Stories:
+As a user I want to:
+- use the app in the latest version of Chrome
+- be able to enter new students
+- search existing students by field
+- see list of all students
+- delete or update a student
+- record unique id, first name, last name, email, grade level
+Choose a K-8 or High School page
+- themes should be different
+- should include students within that grade level range
+
+Technical Requirements:
+- Single page app
+- AngularJS
+- bootstrap styles
+- local storage
+- git
+
--- controller.js
+++ controller.js
@@ -1,8 +1,21 @@
 var myApp = angular.module('mainApp', []);
 
+//////////////
+// Controller
+/////////////
+
 myApp.controller('dataController', function($scope, $http) {
+
+  // set default values
+  
   $scope.order = "lastName";
   $scope.student = [];
+  $scope.selection = "0-8";
+  $scope.entries = [];
+  $scope.mode = "create"; // or edit
+
+  // service
+  
   $http.get('content.json')
    .then(function(response) {
     $scope.entries = response.data;
@@ -11,22 +24,31 @@ myApp.controller('dataController', function($scope, $http) {
     }
     $scope.maxId = $scope.entries.length;
   });
+
+  //functions 
+  
   $scope.changeOrder = function(newOrder) {
     order = "-" + newOrder;
   };
+  
   $scope.submit = function() {
     this.student.uniqueId = $scope.maxId;
     $scope.maxId++;
     $scope.entries.push(this.student);
-    this.student = [];
+    changeToEdit(false);
   };
+  
   $scope.delete = function(deleteId) {
-    for (i=0; i<$scope.entries.length; i++) {
-      if ($scope.entries[i].uniqueId == deleteId) {
-        $scope.entries.splice(i, 1);
+    if (window.confirm('Are you sure you want to delete this?')) {
+      editIndex = findIndex(deleteId);
+      if ($scope.entries[editIndex].uniqueId == $scope.student.uniqueId) {
+        $scope.students = [];
+	changeToEdit(false);
       }
+      $scope.entries.splice(editIndex,1);
     }
   };
+  
   $scope.edit = function(editId) {
     editIndex = findIndex(editId);
     $scope.student.lastName = $scope.entries[editIndex].lastName; 
@@ -34,19 +56,22 @@ myApp.controller('dataController', function($scope, $http) {
     $scope.student.gradeLevel = $scope.entries[editIndex].gradeLevel;
     $scope.student.email = $scope.entries[editIndex].email;
     $scope.student.uniqueId = $scope.entries[editIndex].uniqueId;
+    changeToEdit(true);
   };
+  
   $scope.update = function() {
     editIndex = findIndex($scope.student.uniqueId);
     $scope.entries[editIndex].lastName = $scope.student.lastName;
     $scope.entries[editIndex].firstName = $scope.student.firstName;
     $scope.entries[editIndex].gradeLevel = $scope.student.gradeLevel;
     $scope.entries[editIndex].email = $scope.student.email;
-    $scope.student = [];
+    changeToEdit(false);
   };
+  
   $scope.cancel = function() {
-    $scope.student = [];
+    changeToEdit(false);
   };
-
+  
   function findIndex(uid) {
     result = null;
     for (i=0; i<$scope.entries.length; i++) {
@@ -57,19 +82,37 @@ myApp.controller('dataController', function($scope, $http) {
     return result;
   }
 
+  function changeToEdit(newEditState) {
+     $scope.editState = newEditState;
+     if ($scope.editState == false) {
+       $scope.student = [];
+     }
+  }
+
 });
 
-myApp.directive('alterDirective', function() {
+//////////////
+// Directives
+//////////////
+
+myApp.directive('selectDirective', function() {
   return {
     restrict: "E",
-    templateUrl: "alter.html"
+    templateUrl: "select.html"
+  }
+});
+
+myApp.directive('manageDirective', function() {
+  return {
+    restrict: "E",
+    templateUrl: "manage.html"
   };
 });
 
-myApp.directive('displayDirective', function() {
+myApp.directive('viewDirective', function() {
   return {
     restrict: "E",
-    templateUrl: "display.html"
+    templateUrl: "view.html"
   };
 });
 
@@ -80,9 +123,25 @@ myApp.directive('futureDirective', function() {
   };
 });
 
-myApp.controller('EditController', function($scope) {
-  $scope.alert = function() {
-    alert("edit");
+/////////////////
+// Filter
+////////////////
+
+myApp.filter('gradeLevels', function() {
+  return function(items, selection) {
+    //alert("selection " + selection + " number of items " + items.length);
+    var filtered = [];
+    var min = parseInt(selection.split("-")[0]);
+    var max = parseInt(selection.split("-")[1]);
+    var item = null;
+    for (i=0; i<items.length; i++) {
+      item = items[i];
+      grade = item.gradeLevel;
+      if ((grade >= min) && (grade <= max)) {
+        filtered.push(item);
+      }
+    }
+    return filtered;
   };
 });
 
